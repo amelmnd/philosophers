086c14ad6cf1ae2bce857ab5d9f9cd5638b0e1d8
amennad <amennad@student.42nice.fr>
2023-12-15 11:43:57 +0100

    save version

--- boite_a_outils/inclure_in_every_file.md
+++ boite_a_outils/inclure_in_every_file.md
@@ -1,4 +1,3 @@
 A adapter et incure dans chaque fichier
 
-#include "../include/libft/libft.h"
-#include "project.h"
+#include "philo.h"
--- include/all_struc.h
+++ include/all_struc.h
@@ -6,64 +6,80 @@
 /*   By: amennad <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 17:45:35 by amennad           #+#    #+#             */
-/*   Updated: 2023/12/11 18:16:34 by amennad          ###   ########.fr       */
+/*   Updated: 2023/12/11 16:51:51 by amennad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ALL_STRUCT_H
 # define ALL_STRUCT_H
 
-# include "philo.h"
-# include <pthread.h>
-typedef enum e_bool			t_bool;
-typedef enum e_philo_status	t_philo_status;
-typedef struct s_data		t_data;
-typedef struct s_philo		t_philo;
 
-enum						e_bool
+typedef enum e_bool			t_bool;
+// typedef enum e_philo_status	t_philo_status;
+typedef struct s_data		t_data;
+// typedef struct s_philo		t_philo;
+
+enum e_bool
 {
 	FALSE,
 	TRUE
 };
 
-enum						e_philo_status
+// enum e_philo_status
+// {
+// 	THINKING,
+// 	EATING,
+// 	SLEEPING,
+// };
+
+// struct s_philo
+// {
+// 	int				id_philo;
+// 	int				id_forg;
+// 	int				start_eat;
+// 	int				forg_left;
+// 	int				forg_rigth;
+// 	// enum	e_status_forg;
+// 	t_philo_status	philo_status;
+// };
+
+struct s_data
 {
-	THINKING,
-	EATING,
-	SLEEPING,
-	DEAD
+
 };
 
-struct						s_philo
+typedef struct s_philo
 {
-	t_data					*data;
-	pthread_t				thread;
-	t_bool					eating;
-	size_t					start_time;
-	size_t					last_meal;
-	size_t					is_time_to_eat;
-	int						id_philo;
-	int						id_fork;
-	int						start_eat;
-	int						nb_eats;
-	t_philo_status			philo_status;
-	pthread_mutex_t			*l_fork;
-	pthread_mutex_t			*r_fork;
-	int						nb_forks;
-};
+	struct s_data	*data;
+	pthread_t       t1;
+	int             id;
+	int             eat_cont;
+	int             status;
+	int             eating;
+	uint64_t        time_to_die;
+	pthread_mutex_t	lock;
+	pthread_mutex_t	*r_fork;
+	pthread_mutex_t	*l_fork;
+} t_philo;
 
-struct						s_data
+typedef struct s_data
 {
-	size_t					time_to_die;
-	size_t					time_to_eat;
-	size_t					time_to_sleep;
-	size_t					nb_philo;
-	size_t					nb_meals;
-	time_t					start_meat;
-	t_bool					dead;
-	pthread_mutex_t			*str_message;
-	pthread_t *philos_thread; // Tableau de threads == de philos
-	pthread_mutex_t *forks;   // Tableau de mutex == de fourchettes
-};
+	pthread_t       *tid;
+	int				nb_philo;
+	int             philo_num;
+	int             meals_nb;
+	int		nb_eat;
+	int             dead;
+	int             finished;
+	t_philo         *philos;
+	int		time_to_die;
+	int		time_to_eat;
+	int		time_to_sleep;
+	u_int64_t       start_time;
+	pthread_mutex_t *forks;
+	pthread_mutex_t lock;
+	pthread_mutex_t write;
+} t_data;
+
 
 #endif
